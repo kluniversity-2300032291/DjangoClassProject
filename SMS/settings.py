c62571793b8412e27aee5bd57692f94c01b6e2c7
KATAM SAI SADWIIK <2300032291@kluniversity.in>
2024-12-12 07:47:01 +0530

    Update settings.py

--- SMS/settings.py
+++ SMS/settings.py
@@ -139,4 +139,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'saisadwiikkatam@gmail.com'
-EMAIL_HOST_PASSWORD = 'fxaywgljjvirfcmz'
+EMAIL_HOST_PASSWORD = 'Replace with your password'
